e7fe98c5cfa5c318947086432fad7980ce944307
gergodancs <dancs.gergely@gmail.com>
2022-05-19 16:46:50 +0200

    fetch data from wiki success

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import "./styles/movies.css";
 
 const Movies = (props) => {
+  const url = `https://en.wikipedia.org/w/api.php?`;
+  const params = {
+    origin: "*",
+    format: "json",
+    action: "query",
+    prop: "extracts",
+    exchars: 250,
+    exintro: true,
+    explaintext: true,
+    generator: "search",
+    gsrlimit: 1,
+  };
+
+  const fetchDetailsFromWiki = async (title) => {
+    params.gsrsearch = title;
+
+    const response = await axios(url, { params });
+
+    let data = response.data.query.pages;
+    let key = Object.keys(data);
+
+    response && console.log("ez a movi", data[key].extract);
+  };
+
   return (
     <ul>
       {props.movies.map((movie) => {
@@ -11,6 +36,9 @@ const Movies = (props) => {
               <h2>{movie.name}</h2>
               <p>{movie.overview}</p>
               <span>Release date: {movie.releaseDate}</span>
+              <button onClick={() => fetchDetailsFromWiki(movie.name)}>
+                More info
+              </button>
             </div>
           </li>
         );
